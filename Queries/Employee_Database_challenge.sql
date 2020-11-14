26e1dda9644d28191a190e074313ec1a68cbf699
nluna96 <33613552+nluna96@users.noreply.github.com>
2020-11-14 00:58:02 -0600

    Update Employee_Database_challenge.sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,11 +1,6 @@
-Select a.emp_no, first_name, last_name, title, from_date, to_date 
--- INTO retirement_titles
-from employees a 
-join title b on a.emp_no = b.emp_no
-WHERE birth_date between '1952-01-01' and '1955-12-31'
-order by emp_no asc
-;
+Select * from unique_titles;
 
+Select * from mentorship_eligibilty;
 
 Select DISTINCT ON (emp_no) emp_no, first_name, last_name, title 
 --INTO unique_titles
@@ -15,13 +10,20 @@ Select title, count(*)
 --INTO retiring_titles
 from unique_titles group by 1 order by 2 desc;
 
+Select * from unique_titles;
 
-Select DISTINCT ON (emp_no) a.emp_no, first_name, last_name, birth_date, b.from_date, b.to_date, c.title 
---INTO mentorship_eligibilty
+
+Select title, count(*) 
+from mentorship_eligibilty 
+group by 1 order by 2 desc;
+
+
+--Gender by title breakdown
+Select a.gender, b.title, COUNT(b.emp_no) 
 from employees a 
-join dept_employee b on a.emp_no = b.emp_no 
-join title c on a.emp_no = c.emp_no 
-WHERE (birth_date between '1965-01-01' and '1965-12-31') and c.to_date = '9999-01-01' 
-order by emp_no asc, c.to_date desc;
+join mentorship_eligibilty b on a.emp_no = b.emp_no 
+group by 1,2;
 
-Select * from title where emp_no = '12155';
+--Gender breakdown by retiring
+Select gender, count(*) 
+from unique_titles a join employees b on a.emp_no = b.emp_no group by 1;
